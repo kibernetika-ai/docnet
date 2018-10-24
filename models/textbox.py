777adf5e3b7984838b1167698f644021e610718d
Alexander Gunin <guninalexander@gmail.com>
2018-10-24 10:50:50 +0300

    fix model

--- models/textbox.py
+++ models/textbox.py
@@ -3,13 +3,13 @@ import numpy as np
 import math
 import util.textbox_common
 from util import ssd_vgg_preprocessing
-
+import logging
 
 def conv2d(input, outputNum, kernel=[3, 3], strides=[1, 1], padding='SAME', bn=False, trainPhase=True, name='conv2d'):
     with tf.name_scope(name) as scope:
         output = tf.layers.conv2d(input, outputNum, kernel, strides=strides, padding=padding, activation=tf.nn.relu)
         if bn:
-            output = tf.layers.batch_normalization(output, center=False, is_training=trainPhase)
+            output = tf.layers.batch_normalization(output, center=False, training=trainPhase)
         return output
 
 
@@ -48,6 +48,10 @@ def _ssd_losses(logits, localisations,
         sum_loss = []
         for i in range(len(logits)):
             dtype = logits[i].dtype
+            logging.info('logists: {}'.format(logits[i].shape))
+            logging.info('gscores: {}'.format(gscores[i].shape))
+            logging.info('localisations: {}'.format(localisations[i].shape))
+            logging.info('glocalisations: {}'.format(glocalisations[i].shape))
             with tf.name_scope('block_%i' % i):
                 # Determine weights Tensor.
                 pmask = gscores[i] > match_threshold
@@ -112,35 +116,36 @@ def _text_multibox_layer(inputs,
                          normalization=-1):
     net = inputs
     if normalization > 0:
-        net = tf.layers.batch_normalization(net, center=False, is_training=trainPhase)
+        net = tf.layers.batch_normalization(net, center=False, training=trainPhase)
     num_anchors = 6
     num_classes = 2
     num_loc_pred = 2 * num_anchors * 4
 
     if is_last:
-        loc_pred = tf.layers.conv2d(net, num_loc_pred, [1, 1], activation_fn=None, padding='VALID',
-                                    scope='conv_loc')
+        loc_pred = tf.layers.conv2d(net, num_loc_pred, [1, 1], activation=None, padding='VALID',
+                                    name='conv_loc')
     else:
-        loc_pred = tf.layers.conv2d(net, num_loc_pred, [1, 5], activation_fn=None, padding='SAME',
-                                    scope='conv_loc')
+        loc_pred = tf.layers.conv2d(net, num_loc_pred, [1, 5], activation=None, padding='SAME',
+                                    name='conv_loc')
     loc_pred = tf.reshape(loc_pred, loc_pred.get_shape().as_list()[:-1] + [2, num_anchors, 4])
     # Class prediction.
     scores_pred = 2 * num_anchors * num_classes
     if is_last:
-        sco_pred = tf.layers.conv2d(net, scores_pred, [1, 1], activation_fn=None, padding='VALID',
-                                    scope='conv_cls')
+        sco_pred = tf.layers.conv2d(net, scores_pred, [1, 1], activation=None, padding='VALID',
+                                    name='conv_cls')
     else:
-        sco_pred = tf.layers.conv2d(net, scores_pred, [1, 5], activation_fn=None, padding='SAME',
-                                    scope='conv_cls')
+        sco_pred = tf.layers.conv2d(net, scores_pred, [1, 5], activation=None, padding='SAME',
+                                    name='conv_cls')
     sco_pred = tf.reshape(sco_pred, sco_pred.get_shape().as_list()[:-1] + [2, num_anchors, num_classes])
+    logging.info('sco_pred: {}'.format(sco_pred.shape))
     return sco_pred, loc_pred
 
 
 def _box_fn(features, labels, mode, params=None, config=None):
     global_step = tf.train.get_or_create_global_step()
     input = features['image']
-    glocalisations = labels['glocalisations']
-    gscores = labels['glocalisations']
+    glocalisations = list(features['glocalisations'])
+    gscores = list(features['gscores'])
 
     with tf.name_scope(name="vgg") as scope:
         conv1_1 = conv2d(input, 64, name='conv1_1')  # 300
@@ -178,10 +183,10 @@ def _box_fn(features, labels, mode, params=None, config=None):
         for i, layer in enumerate(layers):
             with tf.variable_scope('{}_box'.format(i)):
                 norm = 20 if i == 0 else -1
-                p, l = _text_multibox_layer(layer, mode == tf.estimator.ModeKeys.TRAIN, is_last=(i == len(layers)),
+                sco_pred, loc_pred = _text_multibox_layer(layer, mode == tf.estimator.ModeKeys.TRAIN, is_last=(i == len(layers)),
                                             normalization=norm)
-                logits.append(p)
-                localisations.append(l)
+                logits.append(sco_pred)
+                localisations.append(loc_pred)
     if mode != tf.estimator.ModeKeys.PREDICT:
         total_loss = _ssd_losses(logits, localisations, glocalisations, gscores,
                                  match_threshold=params['match_threshold'],
@@ -230,8 +235,8 @@ class TextBoxEstimator(tf.estimator.Estimator):
 
 
 def anchors(img_shape, dtype=np.float32):
-    feat_shapes = [(38, 38), (19, 19), (10, 10), (5, 5), (3, 3), (1, 1)],
-    anchor_ratios = [1, 2, 3, 5, 7, 10],
+    feat_shapes = [(38, 38), (19, 19), (10, 10), (5, 5), (3, 3), (1, 1)]
+    anchor_ratios = [1, 2, 3, 5, 7, 10]
     scales = [0.2, 0.34, 0.48, 0.62, 0.76, 0.90]
     return textbox_achor_all_layers(img_shape, feat_shapes, anchor_ratios, scales, 0.5, dtype)
 
@@ -284,6 +289,15 @@ def textbox_anchor_one_layer(img_shape,
 
 
 def bboxes_encode(bboxes, anchors,
+                    scope='text_bboxes_encode'):
+    prior_scaling = [0.1, 0.1, 0.2, 0.2]
+    return util.textbox_common.tf_text_bboxes_encode(
+            bboxes, anchors,
+            matching_threshold=0.1,
+            prior_scaling=prior_scaling,
+            scope=scope)
+
+def bboxes_encode_0(bboxes, anchors,
                   scope='text_bboxes_encode'):
     prior_scaling = [0.1, 0.1, 0.2, 0.2]
     r1 = []
@@ -305,9 +319,11 @@ def fake_fn(params, is_training):
     def _data():
         ds = tf.data.Dataset.range(params['batch_size']*10)
         def _fake(_):
-            i = np.zeros([300, 300, 3], dtype=np.float32)
-            b = np.zeros([6, 4], dtype=np.float32)
-            l = np.zeros([6, 1], dtype=np.int32)
+            i = np.random.uniform(low=0.0, high=1.0, size=(300, 300, 3))
+            b = [np.array([i*20+2,i*20+2,i*20+5,i*20+50],dtype=np.float32) for i in range(1)]
+            b = np.stack(b)
+            b /= 300
+            l = np.ones([1, 1], dtype=np.int32)
             return i, b, l
 
         ds = ds.map(_fake)
@@ -315,7 +331,8 @@ def fake_fn(params, is_training):
         def _proccess(i, b, l):
             i, _, b = ssd_vgg_preprocessing.preprocess_for_train(i, l, b, text_shape)
             glocalisations, gscores = bboxes_encode(b, text_anchors)
-            return {'image': i}, {'glocalisations': glocalisations, 'gscores': gscores}
+            logging.info('gscores: {}'.format(gscores))
+            return {'image': i,'glocalisations': tuple(glocalisations), 'gscores': tuple(gscores)},0
 
         ds = ds.map(_proccess)
         ds = ds.apply(tf.contrib.data.batch_and_drop_remainder(params['batch_size']))
