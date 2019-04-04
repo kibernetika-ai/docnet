0a89979173a8ff1b7f54b27be9fe6b46ede7aa01
Alexander Gunin <guninalexander@gmail.com>
2019-04-04 22:57:43 +0300

    try unet

---
+++ unet/model.py
@@ -0,0 +1,159 @@
+import tensorflow as tf
+import numpy as np
+from unet.unet import unet
+from kibernetika.rpt import MlBoardReporter
+import glob
+
+
+def data_fn(params, training):
+    data_set = params['data_set']
+    datasets_files = []
+    for tf_file in glob.iglob(data_set+'/*.record'):
+        datasets_files.append(tf_file)
+    resolution = params['resolution']
+    fresolution = float(resolution)
+    def _input_fn():
+        ds = tf.data.TFRecordDataset(datasets_files, buffer_size=256 * 1024 * 1024)
+        def _draw(xmin,xmax,ymin,ymax):
+            img = np.zeros((resolution,resolution,1),np.float32)
+            for i in range(len(xmin)):
+                y1 = max(0,ymin[i])
+                y2 = min(160,ymax[i])
+                x1 = max(0,xmin[i])
+                x2 = min(160,xmax[i])
+                img[y1[i]:y2[i],x1[i]:x2[i],0]=1
+            return img
+        def _parser(example):
+            features = {
+                'image/encoded':
+                    tf.FixedLenFeature((), tf.string, default_value=''),
+                'image/height':
+                    tf.FixedLenFeature([1], tf.int64),
+                'image/width':
+                    tf.FixedLenFeature([1], tf.int64),
+                'image/object/bbox/xmin':
+                    tf.VarLenFeature(tf.float32),
+                'image/object/bbox/xmax':
+                    tf.VarLenFeature(tf.float32),
+                'image/object/bbox/ymin':
+                    tf.VarLenFeature(tf.float32),
+                'image/object/bbox/ymax':
+                    tf.VarLenFeature(tf.float32)
+            }
+            res = tf.parse_single_example(example, features)
+            img = tf.image.decode_png(res['image/encoded'], channels=3)
+            original_w = tf.cast(res['image/width'][0], tf.int32)
+            original_h = tf.cast(res['image/height'][0], tf.int32)
+            img = tf.reshape(img, [1,original_h, original_w, 3])
+            img = tf.image.resize_images(img,[resolution,resolution])[0]
+            xmin = tf.cast(tf.sparse_tensor_to_dense(res['image/object/bbox/xmin'])*fresolution,tf.int32)
+            xmax = tf.cast(tf.sparse_tensor_to_dense(res['image/object/bbox/xmax'])*fresolution,tf.int32)
+            ymin = tf.cast(tf.sparse_tensor_to_dense(res['image/object/bbox/ymin'])*fresolution,tf.int32)
+            ymax = tf.cast(tf.sparse_tensor_to_dense(res['image/object/bbox/ymax'])*fresolution,tf.int32)
+            mask = tf.py_func(_draw, [xmin,xmax,ymin,ymax], tf.float32)
+            return img,mask
+        ds = ds.map(_parser)
+        if training:
+            ds = ds.shuffle(params['batch_size'] * 2, reshuffle_each_iteration=True)
+        ds = ds.apply(tf.contrib.data.batch_and_drop_remainder(params['batch_size']))
+        if training:
+            ds = ds.repeat(params['num_epochs']).prefetch(params['batch_size'])
+        return ds
+    return _input_fn
+
+
+def _unet_model_fn(features, labels, mode, params=None, config=None, model_dir=None):
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        features = features['image']
+    else:
+        features = tf.reshape(features, [params['batch_size'], params['resolution'], params['resolution'], 3])
+    training = (mode == tf.estimator.ModeKeys.TRAIN)
+    out_chans = 1
+    logits = unet(features, out_chans, params['num_chans'], params['drop_prob'], params['num_pools'], training=training,
+                  unpool_layer=params['unpool'])
+    mask = tf.sigmoid(logits)
+    loss = None
+    train_op = None
+    hooks = []
+    export_outputs = None
+    eval_hooks = []
+    chief_hooks = []
+    metrics = {}
+    if mode != tf.estimator.ModeKeys.PREDICT:
+        flabels = tf.cast(labels, tf.float32)
+        original = features * flabels
+        predicted = features * mask
+        loss = tf.losses.absolute_difference(flabels, mask)
+        mse = tf.losses.mean_squared_error(flabels, mask)
+        nmse = tf.norm(flabels - mask) ** 2 / tf.norm(flabels) ** 2
+
+        global_step = tf.train.get_or_create_global_step()
+        if training:
+            tf.summary.scalar('mse', mse)
+            tf.summary.scalar('nmse', nmse)
+            board_hook = MlBoardReporter({
+                "_step": global_step,
+                "_train_loss": loss,
+                '_train_lr': float(params['lr']),
+                '_train_mse': mse,
+                '_train_nmse': nmse}, every_steps=params['save_summary_steps'])
+            chief_hooks = [board_hook]
+            update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+            with tf.control_dependencies(update_ops):
+                if params['optimizer'] == 'AdamOptimizer':
+                    opt = tf.train.AdamOptimizer(float(params['lr']))
+                else:
+                    opt = tf.train.RMSPropOptimizer(float(params['lr']), params['weight_decay'])
+                train_op = opt.minimize(loss, global_step=global_step)
+
+        tf.summary.image('Src', features, 3)
+        tf.summary.image('Reconstruction', predicted, 3)
+        tf.summary.image('Original', original, 3)
+        hooks = []
+        if not training:
+            metrics['mse'] = tf.metrics.mean(mse)
+            metrics['nmse'] = tf.metrics.mean(nmse)
+    else:
+        export_outputs = {
+            tf.saved_model.signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY: tf.estimator.export.PredictOutput(
+                mask)}
+
+    return tf.estimator.EstimatorSpec(
+        mode=mode,
+        eval_metric_ops=metrics,
+        predictions=mask,
+        training_chief_hooks=chief_hooks,
+        loss=loss,
+        training_hooks=hooks,
+        export_outputs=export_outputs,
+        evaluation_hooks=eval_hooks,
+        train_op=train_op)
+
+
+
+
+class BoxUnet(tf.estimator.Estimator):
+    def __init__(
+            self,
+            params=None,
+            model_dir=None,
+            config=None,
+            warm_start_from=None
+    ):
+        def _model_fn(features, labels, mode, params, config):
+            return _unet_model_fn(
+                features=features,
+                labels=labels,
+                mode=mode,
+                params=params,
+                config=config,
+                model_dir=model_dir,
+            )
+
+        super(BoxUnet, self).__init__(
+            model_fn=_model_fn,
+            model_dir=model_dir,
+            config=config,
+            params=params,
+            warm_start_from=warm_start_from
+        )
---
+++ unet/unet.py
@@ -0,0 +1,74 @@
+import tensorflow as tf
+import logging
+
+def conv_block(input, out_chans, drop_prob, name, pooling, training):
+    with tf.variable_scope("layer_{}".format(name)):
+        out = input
+        for j in range(2):
+            out = tf.layers.conv2d(out, out_chans, kernel_size=3, padding='same', name="conv_{}".format(j + 1))
+            out = tf.layers.batch_normalization(out, training=training, name="bn_{}".format(j + 1))
+            out = tf.nn.relu(out, name="relu_{}".format(j + 1))
+            if training:
+                out = tf.layers.dropout(out, drop_prob, name="dropout_{}".format(j + 1))
+
+        if not pooling:
+            return out
+
+        pool = tf.nn.max_pool(out,
+                                  ksize=[1, 2, 2, 1],
+                                  strides=[1, 2, 2, 1],
+                                  padding='SAME', name='pool')
+        return out, pool
+
+
+def unpool(pool, ind, ksize=[1, 2, 2, 1], name=None):
+    with tf.variable_scope(name) as scope:
+        mask = tf.cast(ind, tf.int32)
+        input_shape = tf.shape(pool, out_type=tf.int32)
+        output_shape = (input_shape[0], input_shape[1] * ksize[1], input_shape[2] * ksize[2], input_shape[3])
+        one_like_mask = tf.ones_like(mask, dtype=tf.int32)
+        batch_shape = tf.concat([[input_shape[0]], [1], [1], [1]], axis=0)
+        batch_range = tf.reshape(tf.range(output_shape[0], dtype=tf.int32), shape=batch_shape)
+        b = one_like_mask * batch_range
+        y = mask // (output_shape[2] * output_shape[3]) % output_shape[1]
+        x = (mask // output_shape[3]) % output_shape[2]
+        feature_range = tf.range(output_shape[3], dtype=tf.int32)
+        f = one_like_mask * feature_range
+        updates_size = tf.size(pool)
+        indices = tf.transpose(tf.reshape(tf.stack([b, y, x, f]), [4, updates_size]))
+        values = tf.reshape(pool, [updates_size])
+        ret = tf.scatter_nd(indices, values, output_shape)
+        return ret
+
+def unet(inputs, out_chans, chans, drop_prob, num_pool_layers,training=True):
+    output, pull = conv_block(inputs, chans, drop_prob, 'down_1', True, training)
+    logging.info('Down_1 - {}'.format(output.shape))
+    down_sample_layers = [output]
+    ch = chans
+    for i in range(num_pool_layers - 1):
+        ch *= 2
+        output, pull = conv_block(pull, ch, drop_prob, 'down_{}'.format(i + 2), True, training)
+        logging.info('Down_{} - {}'.format(i+2,output.shape))
+        down_sample_layers += [output]
+    i+=1
+
+    output = conv_block(pull[0], ch, drop_prob, 'down_{}'.format(i + 2), False, training,unpool_layer)
+    logging.info('Down_{} - {}'.format(i+2,output.shape))
+
+    for i in range(num_pool_layers):
+        down = down_sample_layers.pop()
+        _,w,h,f = down.shape
+        output = tf.layers.conv2d_transpose(output,filters=f,kernel_size=[3, 3],strides=[2, 2],padding='SAME',
+                                                activation=None,
+                                                kernel_initializer=tf.contrib.layers.variance_scaling_initializer(),
+                                                name='unpool_{}'.format(i + 1))
+        output = tf.concat([output, down], 3)
+        logging.info('Up_{} - {}'.format(i+1,output.shape))
+        if i < (num_pool_layers-1):
+            ch //= 2
+        output = conv_block(output, ch, drop_prob, 'up_{}'.format(i + 1), False, training)
+
+    output = tf.layers.conv2d(output, ch, kernel_size=1, padding='same', name="conv_1")
+    output = tf.layers.conv2d(output, out_chans, kernel_size=1, padding='same', name="conv_2")
+    output = tf.layers.conv2d(output, out_chans, kernel_size=1, padding='same', name="final")
+    return output
