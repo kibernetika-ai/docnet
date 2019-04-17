fc27778cfeff142079ea682415d19b624d16b931
Alexander Gunin <guninalexander@gmail.com>
2019-04-16 20:53:49 -0700

    use rotation

--- unet_reader_hook.py
+++ unet_reader_hook.py
@@ -8,13 +8,23 @@ import math
 LOG = logging.getLogger(__name__)
 LOG.setLevel(logging.INFO)
 
-ENGLISH_CHAR_MAP = [
+ENGLISH_CHAR_MAP1 = [
     '#',
     # Alphabet normal
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
     'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
     '0','1','2','3','4','5','6','7','8','9',
     '-',':','(',')','.',',','/','$',
+    "'",
+    " ",
+    '_'
+]
+
+ENGLISH_CHAR_MAP = [
+    '#',
+    # Alphabet normal
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
     "'",
     " ",
     '_'
